626205ce09c5fae3e63279416cd8726fbbcf4413
rzahnpu10 <54541230+rzahnpu10@users.noreply.github.com>
2021-05-18 10:20:18 -0600

    Add files via upload
    Added initial script

---
+++ zotac-open-browsers.ps1
@@ -0,0 +1,23 @@
+﻿$profiles = @('User1','User2','User3','User4','User5','User6','User7','User8','User9','User10')
+$folder = "C:\temp"
+$chrome = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$val = 0
+
+#create folders
+if (!(Test-Path -Path $folder)) {
+    New-Item -Path $folder -ItemType directory
+    }
+
+
+#create user profiles
+
+while ($val -ne 10)
+{
+    #New-Item -Path $folder + "\" + $profiles[$val] -ItemType directory
+    $Path = $folder+ "\" + $profiles[$val]
+    New-Item -Path $Path -ItemType directory
+    Write-Host $Path
+    #Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe --user-data-dir=$Path"
+    Start-Process -FilePath $chrome -ArgumentList "www.zotacstore.com --user-data-dir=$Path"
+    $val++
+    }
